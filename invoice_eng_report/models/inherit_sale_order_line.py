b84edd2e272f8bc364bf530428d591cba6bbb198
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-03-08 04:34:59 -0800

    Add files via upload

---
+++ invoice_eng_report/models/inherit_sale_order_line.py
@@ -0,0 +1,34 @@
+from odoo import models, fields
+
+
+class InheritSaleOrderModel(models.Model):
+    _inherit = 'sale.order'
+
+    def action_confirm(self):
+        result = super().action_confirm()
+        for rec in self.order_line:
+            rec.move_ids.stock_price_per_box = rec.sale_price_per_box
+        return result
+
+
+class InheritSaleOrderLineModel(models.Model):
+    _inherit = 'sale.order.line'
+
+    sale_price_per_box = fields.Char(string='Price Per Box')
+
+    def _prepare_invoice_line(self, **optional_values):
+        res = super()._prepare_invoice_line(**optional_values)
+        res['account_price_per_box'] = self.sale_price_per_box
+        return res
+
+
+class InheritStockPickingModel(models.Model):
+    _inherit = 'stock.move'
+
+    stock_price_per_box = fields.Char(string='Price Per Box')
+
+
+class InheritAccountMoveModel(models.Model):
+    _inherit = 'account.move.line'
+
+    account_price_per_box = fields.Char(string='Price Per Box')
