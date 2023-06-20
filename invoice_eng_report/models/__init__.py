deb47684bc8ebfe4de8527a907c264f2ba8c64da
Shakilkhan8 <48030846+Shakilkhan8@users.noreply.github.com>
2023-06-20 02:02:14 -0700

    Add files via upload

---
+++ invoice_eng_report/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import inherit_sale_order_line
+from . import inherit_customer
---
+++ invoice_eng_report/models/inherit_customer.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+
+class InheritCustomerModel(models.Model):
+    _inherit = 'res.partner'
+
+    customer_code = fields.Char(string='Customer Code')
+    postal_code = fields.Char(string='Postal Code')
