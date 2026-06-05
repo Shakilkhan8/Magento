589f30f5ed8305f3efb1adb5806f30e2fdef6116
azizwazir <61586101+azizwazir@users.noreply.github.com>
2026-06-05 23:09:47 +0500

    sk_odoo2_api uploaded

--- sk_odoo2_api/models/models.py
+++ sk_odoo2_api/models/models.py
@@ -1,3 +1,5 @@
+import re
+
 from odoo import api, fields, models
 import json
 import requests
@@ -31,6 +33,25 @@ class ProductProductInherit(models.Model):
         for rec in self:
             rec.send_product_data()
 
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        res['default_code'] = self.unique_sku_number() + 1
+        return res
+
+
+    def unique_sku_number(self):
+        default_codes = self.env['product.product'].search([]).mapped('default_code')
+        default_codes = [
+                re.sub(r'\D', '', code)
+                for code in default_codes
+                if code and re.sub(r'\D', '', code)
+            ]
+
+        last_default = max([int(code) for code in default_codes])
+        return last_default
+
+
 
     @api.model
     def create(self, vals_list):
@@ -49,10 +70,6 @@ class ProductProductInherit(models.Model):
         res = super().write(vals)
         self.send_product_data()
         return res
-
-    def uniq_sku(self):
-        pass
-
 
     def send_product_data(self):
         for rec in self:
@@ -235,11 +252,31 @@ class ProductProductInherit(models.Model):
 class ProductVariantInherit(models.Model):
     _inherit = "product.product"
 
+    @api.model
+    def create(self, vals):
+        vals['default_code'] = self.unique_sku_number() + 1
+        print(self.unique_sku_number())
+        res = super().create(vals)
+
+        return res
+
     def write(self, vals):
         res = super().write(vals)
         if self.product_tmpl_id:
             self.product_tmpl_id.send_product_data()
         return res
+
+
+    def unique_sku_number(self):
+        default_codes = self.env['product.product'].search([]).mapped('default_code')
+        default_codes = [
+                re.sub(r'\D', '', code)
+                for code in default_codes
+                if code and re.sub(r'\D', '', code)
+            ]
+
+        last_default = max([int(code) for code in default_codes])
+        return last_default
 
 class ResPartnerInherit(models.Model):
     _inherit = "res.partner"
