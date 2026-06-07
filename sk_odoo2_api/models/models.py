2b75f3dbaa9762c7ead70315b6268a7c620f3a96
azizwazir <61586101+azizwazir@users.noreply.github.com>
2026-06-07 21:12:12 +0500

    sk_odoo_api uploaded

--- sk_odoo2_api/models/models.py
+++ sk_odoo2_api/models/models.py
@@ -30,10 +30,10 @@ class ProductProductInherit(models.Model):
 
     api_id = fields.Integer('API ID')
 
-    @api.onchange('qty_available')
-    def _onchange_qty_available(self):
-        for rec in self:
-            rec.send_product_data()
+    # @api.onchange('qty_available')
+    # def _onchange_qty_available(self):
+    #     for rec in self:
+    #         rec.send_product_data()
 
     @api.model
     def default_get(self, fields_list):
@@ -42,56 +42,40 @@ class ProductProductInherit(models.Model):
         return res
 
     def unique_sku_number(self):
-        default_codes = self.env['product.product'].search([]).mapped('default_code')
-        default_codes = [
-                re.sub(r'\D', '', code)
-                for code in default_codes
-                if code and re.sub(r'\D', '', code)
-            ]
-
-        last_default = max([int(code) for code in default_codes])
-        return last_default
-
+        self.env.cr.execute("""
+            SELECT MAX(CAST(regexp_replace(default_code, '\D', '', 'g') AS INTEGER))
+            FROM product_product
+            WHERE regexp_replace(default_code, '\D', '', 'g') <> ''
+        """)
+        result = self.env.cr.fetchone()
+        return result[0] or 0
 
 
     @api.model
     def create(self, vals_list):
         res = super().create(vals_list)
-        self.send_new_product_data(vals={
-            'name': res.name,
-            'template_image': res.image_1920,
-            'barcode': res.barcode,
-            'id': res.id,
+        if not res.api_id:
+            self.send_new_product_data(vals={
+                'name': res.name,
+                'template_image': res.image_1920,
+                'barcode': res.barcode,
+                'id': res.id,
 
-        })
+            })
         return res
 
 
     def write(self, vals):
         res = super().write(vals)
-        self.send_product_data()
+        if not res.api_id:
+            self.send_product_data()
         return res
 
     def send_product_data(self):
         for rec in self:
             api_config = self.env['api.configuration'].sudo().search([], limit=1)
             url = api_config.url + AUTH_URL
-            # if not api_config:
-            #     raise ValidationError('Please create API configuration and add all API required parameters !')
-            #
-            # if not (api_config.url and api_config.db_name and api_config.user_name and api_config.password):
-            #     raise ValidationError('Please add all API required parameters !')
-            #
-            # url = api_config.url + AUTH_URL
-            # payload = {
-            #     "jsonrpc": "2.0",
-            #     "method": "call",
-            #     "params": {
-            #         "db": api_config.db_name,
-            #         "login": api_config.user_name,
-            #         "password": api_config.password,
-            #     }
-            # }
+
             session_id = self.get_session_id()
 
             headers = {
@@ -168,35 +152,16 @@ class ProductProductInherit(models.Model):
         for rec in product:
             api_config = self.env['api.configuration'].sudo().search([], limit=1)
             url = api_config.url + AUTH_URL
-            # if not api_config:
-            #     raise ValidationError('Please create API configuration and add all API required parameters !')
-            #
-            # if not (api_config.url and api_config.db_name and api_config.user_name and api_config.password):
-            #     raise ValidationError('Please add all API required parameters !')
-            #
-            # url = api_config.url + AUTH_URL
-            # payload = {
-            #     "jsonrpc": "2.0",
-            #     "method": "call",
-            #     "params": {
-            #         "db": api_config.db_name,
-            #         "login": api_config.user_name,
-            #         "password": api_config.password,
-            #     }
-            # }
 
             headers = {
                 'Content-Type': 'application/json',
             }
-            if self.get_session_id():
+            session_id = self.get_session_id()
+            if session_id:
 
-                headers['Token_id'] = self.get_session_id()
+                headers['Token_id'] = session_id
                 url = api_config.url + PRODUCT_URL
 
-
-                variant = self.env['product.product'].search([
-                    ('product_tmpl_id', '=', product.id),
-                ])
 
                 variant = self.env['product.product'].search([
                     ('product_tmpl_id', '=', product.id),
@@ -212,9 +177,7 @@ class ProductProductInherit(models.Model):
                             'attribute': line.attribute_id.name,
                             'values': line.value_ids.mapped('name'),
                         })
-
                     for var in variant:
-
                         variant_attributes = []
 
                         for ptav in var.product_template_attribute_value_ids:
@@ -248,7 +211,7 @@ class ProductProductInherit(models.Model):
                             json=payload,
                             headers={
                                 "Content-Type": "application/json",
-                                "Authorization": f"Bearer {self.get_session_id()}",
+                                "Authorization": f"Bearer {session_id}",
                             },
                             timeout=30
                         )
@@ -287,20 +250,20 @@ class ProductVariantInherit(models.Model):
 
     api_id = fields.Integer('API ID')
 
-    @api.model
-    def create(self, vals):
-        vals['default_code'] = self.unique_sku_number() + 1
-        print(self.unique_sku_number())
-        res = super().create(vals)
-
-        return res
-
-
-    def write(self, vals):
-        res = super().write(vals)
-        if self.product_tmpl_id:
-            self.product_tmpl_id.send_product_data()
-        return res
+    # @api.model
+    # def create(self, vals):
+    #     vals['default_code'] = self.unique_sku_number() + 1
+    #     print(self.unique_sku_number())
+    #     res = super().create(vals)
+    #
+    #     return res
+    #
+    #
+    # def write(self, vals):
+    #     res = super().write(vals)
+    #     if self.product_tmpl_id:
+    #         self.product_tmpl_id.send_product_data()
+    #     return res
 
 
     def unique_sku_number(self):
