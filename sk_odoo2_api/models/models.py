c2ebbc2049d0b444576a486c6d519c6d528bc3a1
azizwazir <61586101+azizwazir@users.noreply.github.com>
2026-08-08 16:30:43 +0500

    Update models.py

--- sk_odoo2_api/models/models.py
+++ sk_odoo2_api/models/models.py
@@ -337,13 +337,8 @@ class ProductVariantInherit(models.Model):
         products = super().create(vals_list)
 
         reserved = self.env['store.deleted.sequence'].search([]).mapped('name')
-        seq_list = sorted(
-                    {
-                        int(name)
-                        for name in reserved
-                        if name and str(name).isdigit()
-                    }
-                )
+        seq_list = [rec for rec in reserved if rec != False]
+        seq_list = sorted(list(set(seq_list)))
         next_no = self.unique_sku_number()
         sequence_to_delete = []
 
