5cd041c3f16739595d8ac3757b23a23f21404571
azizwazir <61586101+azizwazir@users.noreply.github.com>
2026-08-08 10:23:30 +0500

    Update controllers.py

--- sk_odoo2_api/controllers/controllers.py
+++ sk_odoo2_api/controllers/controllers.py
@@ -42,7 +42,7 @@ class OdooSyncController(http.Controller):
             order_lines = []
             for line in lines:
                 product = request.env['product.product'].sudo().search(
-                    [('id', '=', line.get('product_id'))], limit=1
+                    [('api_id', '=', line.get('product_id'))], limit=1
                 )
                 if not product:
                     return {
