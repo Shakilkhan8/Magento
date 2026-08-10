192f931de81089927c9a83f61b93765de164e64f
azizwazir <61586101+azizwazir@users.noreply.github.com>
2026-08-10 10:41:14 +0500

    Update models.py

--- sk_odoo2_api/models/models.py
+++ sk_odoo2_api/models/models.py
@@ -107,10 +107,10 @@ class StockPicking(models.Model):
                 )
                 response.raise_for_status()
                 result = response.json()
-                if response.status_code == 200:
-                    return {'status': 'success', 'message': 'Delivery validated success fully !'}
-                else:
-                    return {'status': 'success', 'message': 'Delivery validated success fully !'}
+                # if response.status_code == 200:
+                #     return {'status': 'success', 'message': 'Delivery validated success fully !'}
+                # else:
+                #     return {'status': 'success', 'message': 'Delivery validated success fully !'}
 
             except requests.exceptions.RequestException as e:
                 return {
